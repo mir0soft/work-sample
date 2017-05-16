142d6006c5d33f32c4eda9b21636902d3727f395
Mihran Hakobyan <mihran1@Mihrans-MacBook-Pro.local>
2017-05-16 22:26:29 +0200

    work sample

---
+++ R_code_stats/hakobyan-13.R
@@ -0,0 +1,145 @@
+# ************************************************
+# *                   CStat2017                  *
+# *          Abgabe von Mihran Hakobyan          *
+# *                                              *
+# *             Donnerstag, 12 Uhr               *
+# ************************************************
+
+## ####### ####### #######  AUFGABE 1
+library(klaR)
+library(mlbench)
+library(plyr)
+load("crunchigkeit.RData")
+data("Ionosphere")
+
+# crossvalid - bestimmt mittlere Fehlerklassifikationsrate
+# einer k-fache Kreuzvalidierung von regularisierter Diskriminanzanalyse
+#
+# Input: param - numerischer Vektor (Parameter der Diskriminanzanalyse)
+#         data - Dataframe oder Matrix (Datensatz)
+#          k   - natuerliche Zahl (Parameter der Kreuzvalidierung)
+#
+# Output:  numerische Zahl (Mittlere Fehlerklassifikationsrate)
+
+crossvalid = function(param, data, k){
+  gamma = param[1]
+  lambda = param[2]
+  obs = nrow(data)
+  var = ncol(data)
+  group = list()
+  true.class = list()
+  num.elem = floor(obs / k)
+  i = 1
+  ind = sample(1:obs)
+  while(i <= k){
+    group[[i]] = data[ind[(1 + num.elem * (i - 1)):(i * num.elem)], ]
+    true.class[[i]] = data[ind[(1 + num.elem * (i - 1)):(i * num.elem)],]$Class
+    i = i + 1
+  }
+  r = obs %% k
+  if (r != 0){
+    while(r > 0){
+      ind = sample(1:k, 1)
+      group[[ind]] = rbind(group[[ind]], data[num.elem * k + r, ])
+      true.class[[ind]] = as.factor(c(as.character(true.class[[ind]]),
+        as.character(data[num.elem * k + r,]$Class)))
+      r = r - 1
+    }
+  }
+  misclass.rate = numeric(k)
+  model.param = matrix(nrow = k, ncol = 2)
+  j = k
+  while(j > 0){
+    test = group[[j]]
+    train = do.call(rbind, group[-j])
+    model = rda(Class ~ ., data = train[, 3:var], gamma = gamma, lambda = lambda)
+    pred.class = predict(model, newdata = test[, 3:var])$class
+    misclass.rate[j] = sum(pred.class != true.class[[j]]) / length(pred.class)
+    j = j - 1
+  }
+  return(mean(misclass.rate))
+}
+
+crossvalid(c(1,1), data = Ionosphere, k =  10)
+
+# crossvalid soll nun bzgl. der beiden Parameter optimiert werden
+# optim(c(0.4,0.7), crossvalid, data = Ionosphere, k = 10, method = "SANN")
+# leider keine Konvergenz
+
+######## ####### #######  AUFGABE 2
+
+########  b)
+
+# subsampling - bestimmt mit der Bootstrap Methode das arithmetische Mittel
+# der mittleren quadratische Abweichungen zwischen tatsaechlichen und vorher-
+# gesagten Werten einer linearen Regression
+#
+# Input: data - Dataframe oder Matrix (Datensatz)
+#          B   - natuerliche Zahl (Wiederholungszahl der Bootstrap-Methode)
+#
+# Output: numerische Zahl (Mittel der mittleren quadratsichen Abweichungen)
+
+subsampling = function(data, B){
+  N = nrow(data)
+  bootstrapOnce  = function(){
+    samples = sample(1:N, replace = TRUE)
+    samples = unique(samples)
+    n = length(samples)
+    target.var = names(data)[1]
+    model = lm(as.formula(paste(target.var, " ~ . ")), data[samples, ])
+    pred = predict(model, data[- samples, ])
+    mse = sum((pred - data[- samples, ][1]) ^ 2) / n
+  }
+  MSE = replicate(B, bootstrapOnce())
+  return (sum(MSE) / B)
+}
+
+subsampling(swiss,200)
+
+########  a) und c)
+
+# variableSelect - Bestimmt die beste Variablenmenge des linearen
+# Regressionsmodells mittels Maximierung des R-Quadrat und
+# die beste Variablenmenge mittels Minimierung des Guetemass
+# berechnet durch subsampling
+#
+# Input: data - Dataframe oder Matrix (Datensatz)
+#          B  - natuerliche Zahl (Wiederholungszahl der Bootstrap-Methode)
+#
+# Output: Liste mit beiden Variablenmengen, siehe Funktionskopf
+
+variableSelect = function(data, B){
+  # maximale anzahl an predictors
+  nmax.pred = ncol(data) - 1
+  # Matrix aller moeglichen Variablenmengen
+  bitstring = expand.grid(as.data.frame(matrix(rep(c(1, 0), nmax.pred), 2)))
+  # alle moeglichen Variablenmengen als Listenelemente (Vektoren)
+  var.set = apply(as.matrix(bitstring), 1, function(x) {which(x != 0)})
+  # Die leere Variablenmenge wird entfernt
+  var.set[length(var.set)] = NULL
+  # Modelle zugehoeriger Variablenmengen.
+  # c(1, var + 1) da erste Variable Zielvariable
+  target.var = names(data)[1]
+  MODEL = lapply(var.set, function(var)
+    {lm(as.formula(paste(target.var, " ~ . ")) , data[, c(1, var + 1)])})
+  adj.r.squared = sapply(MODEL, function(model) {summary(model)$adj.r.squared})
+  # Listenelement mit der besten Variablenmenge
+  best.lvar = which.max(adj.r.squared)
+  # Guetemass fuer jede Variablenmenge bestimmen
+  qual.meas = lapply(var.set, function(var) {subsampling(data[, c(1, var + 1)], B)})
+  # Listenelement mit der besten Variablenmenge (mit Bootstrap)
+  best.lvar.bootstrap = which.min(qual.meas)
+  return(list(names(data)[var.set[[best.lvar]] + 1],
+    names(data)[var.set[[best.lvar.bootstrap]] + 1]))
+}
+
+variableSelect(swiss, 200)
+
+
+
+####### d)
+
+# In a) wird die Guete des Modells anhand eines einzigen Modells basierend
+# auf dem gesamten Datensatz gemessen.
+# In c) hingegen wird die Guete des Modells anhand vieler Modelle
+# basierend auf einem Teil des Datensatzes gemesssen.
---
+++ otto_challenge_python/otto_challenge.py
@@ -0,0 +1,184 @@
+#################################################
+####### MIHRAN HAKOBYAN, OTTO CHALLENGE (kaggle)
+####### Task of the Challenge: predict the right class of the data
+####### RUNTIME of the script approx. 5 min
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing, model_selection
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import log_loss, accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+import xgboost as xgb
+
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD, Adam, RMSprop
+
+
+train = pd.read_csv('/Users/mihran1/Documents/python/otto/train.csv')
+test = pd.read_csv('/Users/mihran1/Documents/python/otto/test.csv')
+
+##### SHORT DATA EXPLORATION
+
+print(train.head())
+print(train.describe())
+
+
+X_train = train.drop(['id', 'target'], axis=1)
+y_train = train['target']
+n_classes = len(set(y_train))
+
+le = preprocessing.LabelEncoder()
+le.fit(y_train)
+y_train = le.transform(y_train)
+
+unique, counts = np.unique(y_train, return_counts=True)
+occur = dict(zip(unique, counts))
+
+# absolute frequency
+target_names = ['Class_{}'.format(i) for i in range(1,10)]
+pos = np.arange(len(occur))
+plt.bar(pos, occur.values(), align='center')
+plt.xticks(pos, target_names)
+plt.title('absolute frequency of the classes')
+
+# partly big correlation between individual features
+corr = pd.concat([X_train, pd.Series(y_train, name='target')], axis=1).corr()
+plt.figure(figsize=(7,7))
+sns.heatmap(corr, vmax=1, square=True, xticklabels=False, yticklabels=False, )
+plt.title('correlation between features')
+
+# no correlation between feautres and targets (makes sense, since those are classes)
+plt.figure(figsize=(12,7))
+pos = np.arange(len(corr.target))
+plt.bar(pos, corr.target, align='center')
+plt.xticks(np.arange(0, len(corr.target), 2.0))
+plt.xlabel('feature')
+plt.title('correlation between features and target')
+
+####### MACHINE LEARNING
+
+X_train = np.array(X_train)
+X_train = preprocessing.scale(X_train)
+
+X_test = np.array(test.drop('id', axis=1))
+X_test = preprocessing.scale(X_test)
+
+# Random Forest crossvalidation
+def RF_crossval(X, y, n_splits=5, shuffle=True, random_state=42, n_estimators=120):
+  cv = model_selection.KFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+  print('RANDOM FOREST crossvalidation')
+  for i, (tr, val) in enumerate(cv.split(X)):
+      RF = RandomForestClassifier(n_estimators=n_estimators)
+      RF.fit(X[tr],y[tr])
+      prediction = RF.predict(X[val])
+      prediction_proba = RF.predict_proba(X[val])
+      score_log_loss = log_loss(y[val], prediction_proba)
+      score_accuracy = accuracy_score(y[val], prediction)
+      print('fold ' +  str(i) + ':')
+      print('log-loss: ' + str(score_log_loss))
+      print('Accuray: ' + str(score_accuracy))
+
+# approx. 81% accuracy, 0.59 log-loss
+RF_crossval(X_train, y_train)
+
+# XGBoost crossvalidation
+def XGB_crossval(X, y,  n_splits=5, shuffle=True, random_state=42, eta=0.1, max_depth=3, n_estimators=100):
+  cv = model_selection.KFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+  print('XGBOOST')
+  for i, (tr, val) in enumerate(cv.split(X)):
+      XGB = xgb.XGBClassifier()
+      XGB.fit(X[tr],y[tr])
+      prediction_proba = XGB.predict_proba(X[val])
+      prediction = XGB.predict(X[val])
+      score_log_loss = log_loss(y[val], prediction_proba)
+      score_accuracy = accuracy_score(y[val], prediction)
+      print('fold ' +  str(i) + ':')
+      print('log-loss: ' + str(score_log_loss))
+      print('Accuray: ' + str(score_accuracy))
+
+# approx. 77% accuracy, 0.65 log-loss
+XGB_crossval(X_train, y_train)
+
+####### NEURAL NET
+
+def NN(X,y,epochs=10, validation_split=0.1):
+
+  onehot_label = to_categorical(y, num_classes= 9)
+
+  model = Sequential([
+      Dense(100, activation='relu', input_dim=93),
+      Dropout(rate=0.5),
+      #Dense(50, activation='relu'),
+      #Dropout(rate=0.5),
+      Dense(9, activation='softmax'),
+  ])
+
+  model.compile(optimizer='Adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+  print('NEURAL NET')
+  model.fit(X, onehot_label, epochs=epochs, validation_split=validation_split)
+
+# Bad performance with NN (approx 18% ), despite several configurations (number layers, number neurons). Overfitting is a big issue here
+NN(X_train, y_train)
+
+#### BAGGING
+# Very good performance with approx. 0.49 log-loss
+
+def bagging(X, y, test_size=0.2, random_state=13):
+  X, X_val, y, y_val = model_selection.train_test_split(X,y, test_size=test_size, random_state=random_state)
+  trind = list(range(len(y)))
+  pred = np.empty([X_val.shape[0], n_classes])
+  B = 10 # number of bagging rounds, should be much higher (runtime increases)
+  tmpL = train.shape[0]
+  print('BAGGING')
+
+  for i in range(B):
+      print('Bagging progress: ' +  str(i))
+      # bootstrapped sample
+      tmpS1 = np.random.choice(trind, len(trind), replace=True)
+      # bootstrapped rest
+      tmpS2 = list(set(trind) - set(tmpS1))
+
+      tmpX2 = X[tmpS2] # bootstrapped rest train
+      tmpY2 = y[tmpS2] #  bootstrapped rest train
+
+      rf = RandomForestClassifier()
+      rf.fit(tmpX2, tmpY2)
+
+      tmpX1 = X[tmpS1] # bootstrapped train
+      tmpY1 = y[tmpS1] # bootstrapped train
+
+      tmpX2 = rf.predict_proba(tmpX1) # new feautres from boostrapped  train
+      tmpX3 = rf.predict_proba(X_val) # new feature from X_val
+
+      XGB = xgb.XGBClassifier(max_depth=11, learning_rate=0.1, min_child_weight=10, objective = "multi:softprob", nthread=7)
+      XGB.fit(np.concatenate((tmpX1, tmpX2), axis=1), tmpY1)
+
+      pred0 = XGB.predict_proba(np.concatenate((X_val, tmpX3), axis=1))
+      pred = pred + pred0
+
+  pred = pred/float(B)
+  print('logloss: ' + str(log_loss(y_val, pred)))
+
+# No call due to the long runtime
+'''bagging(X_train, y_train)'''
+
+plt.show()
+
+# SUBMISSION
+# We will take Random Forest, retrain on the whole set and make predictions
+
+RF = RandomForestClassifier(n_estimators=120)
+RF.fit(X_train, y_train)
+prediction_proba = RF.predict_proba(X_test)
+sub = pd.DataFrame(prediction_proba)
+sub.insert(loc=0, column='id', value=test['id'])
+sub.columns = ['id'] + target_names
+sub.to_csv("submission_rf.csv", index=False)
